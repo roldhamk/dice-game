cdee5e1eef8150c63565bdb07b692a0e91c24d61
Rachael Oldham-Knott <roldhamk@gmail.com>
2020-02-20 11:26:42 +0000

    fixed if/else and dice change image

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const image = document.getElementsByClassName("image");
 const button = document.getElementById("roll-btn");
 const number = document.getElementById("number");
+const image = document.getElementById("dice");
+
+let sum = 0;
 
 button.addEventListener("click", () => {
   let num = Math.floor(Math.random() * 6 + 1);
-  if (num === 1) {
-    number.innerText = "You lose!";
+  sum += num;
+  button.innerText = "Roll";
+  number.innerText = sum;
+  image.style.display = "block";
+  image.src = `./img/dice${num}.png`;
+  if (sum >= 20) {
+    number.innerText = `you win!`;
+    button.innerText = "Play again";
+    sum = 0;
+  } else if (num === 1) {
+    number.innerText = `you lost!`;
+    sum = 0;
+    button.innerText = "Restart Game";
   }
 });
